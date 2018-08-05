26f9f504dc4321066f7f2355da74a07a0b327ca4
wangxifa <1007026946@qq.com>
2018-08-05 21:56:09 +0800

    common header

---
+++ jianshu/src/common/header/style.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+import logoPic from '../../statics/logo.png';
+
+export const HeaderWrapper = styled.div`
+    position: relative;
+    height: 56px;
+    border-bottom: 1px solid #f0f0f0;
+`;
+
+export const Logo = styled.a.attrs({
+    href: '/'
+})`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100px;
+    height: 56px;
+    background: url(${logoPic});
+    background-size: contain;
+`;
+
+export const Nav = styled.div`
+    width: 960px;
+    padding-right: 70px;
+    box-sizing: border-box;
+    height: 100%;
+    margin: 0 auto;
+`;
+
+export const NavItem = styled.div`
+    left-height: 56px;
+    padding: 20px 15px;
+    font-size: 17px;
+    color: #333;
+    &.left {
+        float: left;
+    }
+    &.right {
+        float: right;
+        color: #969696;
+    }
+    &.active {
+        color: #ea6f5a;
+    }
+`;
+export const NavSearch = styled.input.attrs({
+    placeholder: "搜索"
+})`
+    width: 160px;
+    height: 38px;
+    padding: 0 30px 0 20px;
+    box-sizing: border-box;
+    margin-top: 9px;
+    margin-left: 20px;
+    border: none;
+    outline: none;
+    border-radius: 19px;
+    background: #eee;
+    font-size: 14px;
+    color: #666;
+    &::placeholder {
+        color: #999;
+    }
+`;
+export const Addition = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 56px;
+`;
+export const Button = styled.div`
+    float: right;
+    line-height: 38px;
+    margin-top: 9px;
+    margin-right: 20px;    
+    padding: 0 20px;
+    border-radius: 19px;
+    font-size: 14px;
+    hight: 40px;
+    &.reg {
+        color: #ec6149;
+        border: 1px solid rgba(236,97,73,.7);
+    }
+    &.writting {
+        color: #fff;
+        background: #ec6149;
+    }
+`;
