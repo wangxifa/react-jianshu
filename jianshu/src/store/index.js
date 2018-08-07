fc548704a115a5a385b9ea3790132fae3f16473b
wangxifa <1007026946@qq.com>
2018-08-07 12:25:59 +0800

    旋转功能的完成

--- jianshu/src/store/index.js
+++ jianshu/src/store/index.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
-import reducer from './reducer'
+import { createStore, compose, applyMiddleware } from 'redux';
+import reducer from './reducer';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer,composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 export default store;
 
--- jianshu/src/store/reducer.js
+++ jianshu/src/store/reducer.js
@@ -1,18 +1,8 @@
-const defaultState = {
-    focused: false
-};
+import { combineReducers } from 'redux-immutable';
+import { reducer as headerReducer } from '../common/header/store';
 
-export default (state=defaultState, action) => {
-    if(action.type === 'search_focus') {
-        return {
-            focused: true
-        }
-    }
-    if(action.type === 'search_blur') {
-        return {
-            focused: false
-        }
-    }
+const reducer =  combineReducers({
+    header: headerReducer
+})
 
-    return state;
-}
+export default reducer;
